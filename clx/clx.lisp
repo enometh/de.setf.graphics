bdbc4f45ad2c9a4f1c0123f6c6737440fad49373
Madhu <enometh@net.meer>
2025-08-21 17:02:41 +0530

    clx/clx.lisp: (clx-controller-dispatch-event): handle :no-exposure

--- clx/clx.lisp
+++ clx/clx.lisp
@@ -276,6 +276,8 @@ store for windows." )
 
 
 (defgeneric clx-controller-dispatch-event (controller context event &key &allow-other-keys)
+  (:method ((controller clx-controller) (context clx-context) (event (eql :no-exposure))
+            &key window))
   (:method ((controller clx-controller) (context clx-context) (event (eql :exposure))
             &key window)
            (context-expose-event-handler controller context window))
